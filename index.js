6aef938c7dc10ee759d50374c23804c484c135da
Abayomi Aishida <yomicorperate@gmail.com>
2023-03-07 16:14:30 +0100

    Building an object oriented program

--- index.js
+++ index.js
@@ -64,3 +64,59 @@ function styleAndCelebrate(color, background, fontSize, txt, reason) {
 // Call styleAndCelebrate
 styleAndCelebrate("ef7c8e", "fae8e0", "30px", "You made it!", "champions");
 ``;
+
+// OOP Project
+// Task 1: Code a Person class
+class Person {
+  constructor(name = "Tom", age = 20, energy = 100) {
+    this.name = name;
+    this.age = age;
+    this.energy = energy;
+  }
+
+  sleep() {
+    this.energy += 10;
+  }
+
+  doSomethingFun() {
+    this.energy -= 10;
+  }
+}
+// Task 2: Code a Worker class
+class Worker extends Person {
+  constructor(name, age, energy, xp = 0, hourlyWage = 10) {
+    super(name, age, energy);
+    this.xp = xp;
+    this.hourlyWage = hourlyWage;
+  }
+
+  goToWork() {
+    this.xp += 10;
+  }
+}
+
+// Task 3: Code an intern object, run methods
+function intern() {
+  const intern1 = new Worker(
+    (name = "Bob"),
+    (age = 21),
+    (energy = 110),
+    (xp = 0),
+    (hourlyWage = 10)
+  );
+  intern1.goToWork();
+  return intern1;
+}
+
+// Task 4: Code a manager object, methods
+function manager() {
+  const manager1 = new Worker(
+    (name = "Alice"),
+    (age = 30),
+    (energy = 120),
+    (xp = 100),
+    (hourlyWage = 30)
+  );
+  manager1.doSomethingFun();
+  return manager1;
+}
